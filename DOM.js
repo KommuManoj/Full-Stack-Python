133d5251ea0b146be66d93e9ffd4064e210f483a
KommuManoj <kmanojsince2004@gmail.com>
2025-06-25 15:14:29 +0530

    HTML with JS

---
+++ DOM.js
@@ -0,0 +1,7 @@
+document.title="DOM in JavaScript";
+document.body.style.backgroundColor="lightblue";
+document.getElementsByClassName("ds")[0].style.color="red";
+document.getElementsByClassName("ds")[1].style.color="green";
+document.getElementsByClassName("ds")[2].style.color="blue";
+document.getElementsByClassName("ds")[3].style.color="yellow";
+document.getElementsByClassName("ds")[4].style.color="orange";
